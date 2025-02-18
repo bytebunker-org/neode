a21e1414816d2c1f9e03770448392e4eed8f79e7
Moritz Hein <moritz.hein@live.de>
2025-02-18 22:12:37 +0100

    fix: relationship enums where not exported

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,9 @@ export { NodeCollection } from "./NodeCollection.js";
 export { Property } from "./Property.js";
 export { Relationship } from "./Relationship.js";
 export { RelationshipCollection } from "./RelationshipCollection.js";
-export { RelationshipType } from "./RelationshipType.js";
+export {
+	RelationshipType,
+	RelationshipDirectionEnum,
+	RelationshipCascadePolicyEnum,
+} from "./RelationshipType.js";
 export { Schema } from "./Schema.js";
